2511d0473a87ae7eb341f63076590b091dbd5729
Vojtěch Kalivoda <kalivoda167@gmail.com>
2023-02-21 21:26:44 +0100

    Initial Commit

---
+++ NetflixSeries/NetflixSeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixSeriesApp.swift
+//  NetflixSeries
+//
+//  Created by Vojtěch Kalivoda on 21.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetflixSeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
